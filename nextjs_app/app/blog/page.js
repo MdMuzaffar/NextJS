7c87e123d74942f40466f35fa9ecf64facd5a2f1
MdMuzaffar <54287360+MdMuzaffar@users.noreply.github.com>
2026-04-10 23:51:05 +0530

    Fetch data from API
    Fetch data from external API to show in UI

--- nextjs_app/app/blog/page.js
+++ nextjs_app/app/blog/page.js
@@ -1,26 +1,29 @@
 import Link from "next/link";
-export default function blogPage() {
-  const post = [
-    {
-      id: 1,
-      title: "First Post",
-      content: "This is the content of the first post.",
-    },
-    {
-      id: 2,
-      title: "Second Post",
-      content: "This is the content of the second post.",
-    },
-  ];
+export default async function blogPage() {
+  const data = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await data.json();
+
+  // const post = [
+  //   {
+  //     id: 1,
+  //     title: "First Post",
+  //     content: "This is the content of the first post.",
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "Second Post",
+  //     content: "This is the content of the second post.",
+  //   },
+  // ];
   return (
     <div>
       <h1>Blog Page</h1>
-      {post.map((p) => (
+      {posts.map((p) => (
         <div key={p.id}>
           <Link href={`/blog/${p.id}`} rel="canonical">
-            <h2>{p.title}</h2>
+            <h2 className="text-sm font-bold mb-2 text-blue-600">{p.title}</h2>
           </Link>
-          <p>{p.content}</p>
+          <p className="text-black-600">{p.body}</p>
         </div>
       ))}
     </div>
